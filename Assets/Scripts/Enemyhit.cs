85f7b5a517f7c73e142700c694a39bb472521442
Carter Elliot Weinberg <weinberg.c@northeastern.edu>
2017-04-23 13:49:09 -0400

    Added a restart button if the player gets hit with a bullet

--- Assets/Scripts/Enemyhit.cs
+++ Assets/Scripts/Enemyhit.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Enemyhit : MonoBehaviour {
 
-	//destroys the gameobject that hs been collided with
-	void OnCollisionEnter(Collision collision) {
+    public Transform respawnpoint;
+    GameObject restart;
+
+    void Awake()
+    {
+        restart = GameObject.FindGameObjectWithTag("RestartButton");
+        restart.SetActive (false);
+    }
+
+
+    //destroys the gameobject that hs been collided with
+    void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.CompareTag ("Bullet")) {
 			//print ("collision has occured");
 			Destroy (collision.gameObject);
 			gameObject.SetActive (false);
-		} 		
+            ActivateRestartButton();
+        } 		
 	}
+
+    //Activate the restart button
+    void ActivateRestartButton () {
+        restart.SetActive (true);
+    }
+
+    //Respawn the player if the restart button is pressed
+    public void Respawn() {
+        this.transform.position = respawnpoint.position;
+        gameObject.SetActive(true);
+        restart.SetActive(false);
+    }
 
 }
