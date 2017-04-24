cc2beddc9bdceeaebc4eca6967af92ab0e0b5a5c
Carter.Weinberg <weinbergcarter@gmail.com>
2017-04-23 21:40:08 -0400

    I added the levelporter 3d model

---
+++ Assets/Scripts/CollectableRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableRotate : MonoBehaviour {
+
+	GameObject collectable;
+
+	void Start () {
+		collectable = this.gameObject;
+	}
+	
+	//Rotate the collectable to the right
+	//I think this is a good use of fixedupdate?
+	void FixedUpdate () {
+		collectable.transform.Rotate(Vector3.right * Time.deltaTime);
+	}
+}
